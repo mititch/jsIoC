fe88ccfd93b5b614ef8e3732103e5358ad8c0ed6
mititch <mititch@live.com>
2013-05-07 19:02:14 +0300

    Update ctors.js

--- ctors.js
+++ ctors.js
@@ -1,14 +1,16 @@
 function CtorA (cont) {
     this.objB = "interfaceB";
-    this.objB = new cont[this.objB](cont);
+    this.objB = cont.Instance[this.objB](cont);
 
     this.doA = function () {
         this.objB.doB();
     };
 }
+CtorA.interface = "interfaceA";
     
 function CtorB(cont) {
     this.doB = function() {
         alert('I am B!');
     };
 }
+CtorB.interface = "interfaceB";
